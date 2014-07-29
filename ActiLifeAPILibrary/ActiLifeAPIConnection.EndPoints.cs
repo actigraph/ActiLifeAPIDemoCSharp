8dcdca13d977a128692031eb841ae8f2424c2182
Daniel Judge <dwjref@gmail.com>
2014-07-29 13:52:39 -0500

    Added data scoring end point

--- ActiLifeAPILibrary/ActiLifeAPIConnection.EndPoints.cs
+++ ActiLifeAPILibrary/ActiLifeAPIConnection.EndPoints.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ActiLifeAPILibrary.Models.Actions;
+using Newtonsoft.Json;
 
 namespace ActiLifeAPILibrary
 {
@@ -10,7 +12,19 @@ namespace ActiLifeAPILibrary
 	{
 		async public Task<string> GetActiLifeVersion()
 		{
-			return await SendData(new ActiLifeAPILibrary.Models.Request.RequestBase { Action = "ActiLifeVersion" }.ToJson());
+			return await SendData(new Models.Request.RequestBase { Action = "ActiLifeVersion" }.ToJson());
 		}
+
+	    public async Task<string> GetDataScoringResults(string fileName)
+        {
+            DataScoring d = new DataScoring
+            {
+                FileInputPath = fileName,
+                CalculateEnergyExpenditure = true,
+                ResultOptions = new ResultOptions {IncludeTotalResults = true}
+            };
+
+	        return await SendData(new Models.Request.RequestBase {Action = "DataScoring", Args = JsonConvert.SerializeObject(d, Formatting.Indented)}.ToJson());
+        }
 	}
 }
