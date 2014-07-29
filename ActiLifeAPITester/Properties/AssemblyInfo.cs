d4d2dc64f0e9592c9655544597b24db77c13da3e
Derek Gates <derek@derekgates.us>
2014-07-29 15:28:19 -0500

    Assembly info; add product name, version, copyright.

--- ActiLifeAPITester/Properties/AssemblyInfo.cs
+++ ActiLifeAPITester/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ActiLifeAPITester")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ActiLife API Tester")]
+[assembly: AssemblyDescription("GUI that tests ActiLife API from ActiGraph.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ActiLifeAPITester")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("ActiGraph, LLC")]
+[assembly: AssemblyProduct("ActiLife")]
+[assembly: AssemblyCopyright("Copyright © 2013-2014 ActiGraph, LLC. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
