9f3cf03bed8c1e35a71c10326c140d2a460c5d70
Derek Gates <derek@derekgates.us>
2014-07-29 12:35:43 -0500

    Add custom APIConnectionException
    - Catch exceptions for connection, send/receive and throw custom
    exception that wraps the initial exception.

--- ActiLifeAPILibrary/ActiLifeAPIConnection.cs
+++ ActiLifeAPILibrary/ActiLifeAPIConnection.cs
@@ -21,11 +21,15 @@ namespace ActiLifeAPILibrary
 		{
 			Trace.WriteLine("Connecting to ActiLife...");
 
-			_pipe = new NamedPipeClientStream(".", "actilifeapi", PipeDirection.InOut);
-			_pipe.Connect();
-			_pipe.ReadMode = PipeTransmissionMode.Message; //important!
+			try
+			{
+				_pipe = new NamedPipeClientStream(".", "actilifeapi", PipeDirection.InOut);
+				_pipe.Connect();
+				_pipe.ReadMode = PipeTransmissionMode.Message; //important!
 
-			Trace.WriteLine("Connected!");
+				Trace.WriteLine("Connected!");
+			}
+			catch (Exception ex) { throw new Exceptions.APIConnectionException("Unable to connect. \"" + ex.Message + "\"", ex); }
 		}
 
 		public bool IsConnected
@@ -39,11 +43,15 @@ namespace ActiLifeAPILibrary
 			{
 				Trace.WriteLine("Sending " + json);
 
-				//going to assume unicode here because we might have out of US customers using the API
-				byte[] replyBytes = Encoding.UTF8.GetBytes(json);
+				try
+				{
+					//going to assume unicode here because we might have out of US customers using the API
+					byte[] replyBytes = Encoding.UTF8.GetBytes(json);
 
-				_pipe.Write(replyBytes, 0, replyBytes.Length);
-				_pipe.WaitForPipeDrain();
+					_pipe.Write(replyBytes, 0, replyBytes.Length);
+					_pipe.WaitForPipeDrain();
+				}
+				catch (Exception ex) { throw new Exceptions.APIConnectionException("Unable to send data. \"" + ex.Message + "\"", ex); }
 
 				return await ReceiveData();
 			}
@@ -56,16 +64,20 @@ namespace ActiLifeAPILibrary
 			{
 				StringBuilder mb = new StringBuilder();
 
-				// read one message from the server
-				int byteCount = 0;
-				byte[] buff = new byte[1024];
-				do
+				try
 				{
-					byteCount = _pipe.Read(buff, 0, buff.Length);
+					// read one message from the server
+					int byteCount = 0;
+					byte[] buff = new byte[1024];
+					do
+					{
+						byteCount = _pipe.Read(buff, 0, buff.Length);
 
-					if (byteCount != 0)
-						mb.Append(System.Text.Encoding.UTF8.GetString(buff, 0, byteCount));
-				} while (IsConnected && !_pipe.IsMessageComplete);
+						if (byteCount != 0)
+							mb.Append(System.Text.Encoding.UTF8.GetString(buff, 0, byteCount));
+					} while (IsConnected && !_pipe.IsMessageComplete);
+				}
+				catch (Exception ex) { throw new Exceptions.APIConnectionException("Unable to send data. \"" + ex.Message + "\"", ex); }
 
 				Trace.WriteLine("Received " + mb.ToString());
 
---
+++ ActiLifeAPILibrary/Exceptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ActiLifeAPILibrary
+{
+	public class Exceptions
+	{
+		/// <summary>
+		/// Exception thrown for all API Connection exceptions.
+		/// </summary>
+		public class APIConnectionException : System.IO.IOException
+		{
+			public APIConnectionException() : base()
+			{
+
+			}
+			public APIConnectionException(string message) : base(message)	
+			{
+
+			}
+			public APIConnectionException(string message, Exception innerException):base(message, innerException)
+			{
+
+			}
+		}
+	}
+}
