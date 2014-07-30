14f938c86c44804493114021eaa3723750f8a659
Derek Gates <derek@derekgates.us>
2014-07-30 16:40:13 -0500

    RequestBase to fill property classes/default values like ActionBase.

--- ActiLifeAPILibrary/Models/Request/RequestBase.cs
+++ ActiLifeAPILibrary/Models/Request/RequestBase.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using Newtonsoft.Json;
 
@@ -11,6 +13,31 @@ namespace ActiLifeAPILibrary.Models.Request
 	/// </summary>
 	public class RequestBase
 	{
+		public RequestBase()
+		{
+			//find all properties that have classes and construct them.
+			foreach (PropertyInfo item in (from prop in this.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+										   where prop.CanWrite && prop.GetType().IsClass && prop.PropertyType.GetConstructor(Type.EmptyTypes) != null
+										   select prop))
+			{
+				var constructor = item.PropertyType.GetConstructor(Type.EmptyTypes);
+
+				item.SetValue(this, constructor.Invoke(Type.EmptyTypes), null);
+			}
+
+
+			//now set all default values from DefaultValue attribute
+			foreach (PropertyInfo item in (from prop in this.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+										   where prop.GetCustomAttributes(typeof(System.ComponentModel.DefaultValueAttribute), true).Length != 0
+										   select prop))
+			{
+				foreach (var attribute in item.GetCustomAttributes(typeof(System.ComponentModel.DefaultValueAttribute), true))
+				{
+					item.SetValue(this, ((DefaultValueAttribute)attribute).Value, null);
+				}
+			}
+		}
+
 		/// <summary>
 		/// Action of the request (the endpoint).  Populated by default to the type's Name.  Such as 'public class DataScoring' would be Action of 'DataScoring'.
 		/// </summary>
