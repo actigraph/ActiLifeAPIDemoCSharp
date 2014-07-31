b054dd10c2029f7896a256e0e46c3ba6c3565d55
Daniel Judge <dwjref@gmail.com>
2014-07-31 16:32:21 -0500

    Allow user to add age & weight for METs
    Closes #19
    Closes #20

--- ActiLifeAPILibrary/Models/Actions/METOptions.cs
+++ ActiLifeAPILibrary/Models/Actions/METOptions.cs
@@ -10,5 +10,20 @@ namespace ActiLifeAPILibrary.Models.Actions
         [JsonProperty(Required = Required.Default, DefaultValueHandling = DefaultValueHandling.Populate)]
         [DefaultValue("FreedsonEEAdult")]
         public virtual string Algorithm { get; set; }
+
+        /// <summary> Weight of the subject in kilograms. </summary>
+        /// <para></para>
+        /// <para>Note:</para>
+        /// <para>- Only required for FreedsonEEAdult algorithm.</para>
+        [JsonProperty(Required = Required.Default, DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public virtual float Weight { get; set; }
+
+        /// <summary> Age of the subject in kilograms. </summary>
+        /// <para></para>
+        /// <para>Note:</para>
+        /// <para>- Only required for FreedsonChildren algorithm.</para>
+        /// <para>- Ages of 6-18 required for FreedsonChildren algorithm</para>
+        [JsonProperty(Required = Required.Default, DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public virtual int Age { get; set; }
     }
 }
