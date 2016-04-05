f2fd95de3fae787fc9a16ded08f4ca045594b7de
Derek Gates <derek@derekgates.us>
2016-04-05 13:52:34 -0500

    Version bump
    - 2.5.0 release
    - 2016 copyright

--- ActiLifeAPITester/Properties/AssemblyInfo.cs
+++ ActiLifeAPITester/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ActiGraph, LLC")]
 [assembly: AssemblyProduct("ActiLife API")]
-[assembly: AssemblyCopyright("Copyright © 2013-2014 ActiGraph, LLC. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2013-2016 ActiGraph, LLC. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
